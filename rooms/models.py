c1ac3e42da02b8d77dd5cb440dedb87471a7c33f
kih1024 <rladlsgh654@naver.com>
2020-01-10 14:45:55 +0900

    Change Admin of Rooms display

--- rooms/models.py
+++ rooms/models.py
@@ -84,13 +84,22 @@ class Room(core_models.TimeStampedModel):
 
     def __str__(self):
         return self.name
+        
+    def total_rating(self):
+        all_reviews = self.reviews.all()
+        all_ratings = 0
+        for review in all_reviews:
+            all_ratings += review.rating_average()
+        return all_ratings / len(all_reviews)
+
+    
 
 
 class Photo(core_models.TimeStampedModel):
     """ Photo Model Definition """
 
     caption = models.CharField(max_length=80)
-    file = models.ImageField()
+    file = models.ImageField(upload_to="room_photos")
     room = models.ForeignKey(Room, related_name="photos", on_delete=models.CASCADE)
 
     def __str__(self):
