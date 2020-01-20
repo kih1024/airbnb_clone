51001ab319b93f0beb39838fa016b9c439945ff6
kih1024 <rladlsgh654@naver.com>
2020-01-20 14:16:58 +0900

    Add rooms.views

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,15 @@
+from . import models
 from django.shortcuts import render
+
+# from django.http import HttpResponse
+
+
+def all_rooms(request):
+    # now = datetime.now()
+    # hungry = True
+    # return HttpResponse(content=f"<h1>{now}</h1>")
+    all_rooms = models.Room.objects.all()
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})
+
 
 # Create your views here.
