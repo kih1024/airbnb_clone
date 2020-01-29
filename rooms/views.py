3a8d000a214386d8caa7f8c769bc9b0847182f75
kih1024 <rladlsgh654@naver.com>
2020-01-30 00:12:29 +0900

    Use paginator for pagination

--- rooms/views.py
+++ rooms/views.py
@@ -1,32 +1,35 @@
 from math import ceil
 from . import models
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 # from django.http import HttpResponse
 
 
 def all_rooms(request):
-    # now = datetime.now()
-    # hungry = True
-    # return HttpResponse(content=f"<h1>{now}</h1>")
-    # print(request.GET)
-    page = int(request.GET.get("page", 1))
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
-    return render(
-        request,
-        "rooms/home.html",
-        context={
-            "rooms": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count + 1),
-        },
-    )
+    page = request.GET.get("page")
+    room_list = (
+        models.Room.objects.all()
+    )  # 이렇게 하면 쿼리셋만 만드는거지 데이터베이스에서 불러오는게 아님. 호출할때 그때서야 디비에서 값들을 불러옴. 매우 게으름..
+    paginator = Paginator(room_list, 10)
+    rooms = paginator.get_page(page)
+    # print(rooms)
+    print(vars(rooms))
+    return render(request, "rooms/home.html", {"rooms": rooms})
+
+    # print(vars(rooms.paginator))
+    # page = int(page or 1)
+    # page_size = 10
+    # limit = page_size * page
+    # offset = limit - page_size
+    # all_rooms = models.Room.objects.all()[offset:limit]
+    # page_count = ceil(models.Room.objects.count() / page_size)
+    # context={
+    #     "rooms": all_rooms,
+    #     "page": page,
+    #     "page_count": page_count,
+    #     "page_range": range(1, page_count + 1),
+    # },
 
 
 # Create your views here.
