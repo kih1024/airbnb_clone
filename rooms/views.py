6dfe83519dfdabfcf39381932835201b2531b93d
kih1024 <rladlsgh654@naver.com>
2020-02-07 17:17:52 +0900

    Add Search about city,coutries, roomtypes

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,7 @@
-from . import models
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render
+from django_countries import countries
+from . import models
 
 # from django.http import Http404
 # from django.shortcuts import render
@@ -38,9 +39,14 @@ class RoomDetail(DetailView):
 
 
 def search(request):
-    city = request.GET.get("city")
+    city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
-    return render(request, "rooms/search.html", {"city": city})
+    room_types = models.RoomType.objects.all()
+    return render(
+        request,
+        "rooms/search.html",
+        {"city": city, "countries": countries, "room_types": room_types},
+    )
 
 
 # def room_detail(request, pk):
