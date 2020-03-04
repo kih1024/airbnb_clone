922b6f24b64ef3c7a6d7b2af72c646f80b8da469
kih1024 <rladlsgh654@naver.com>
2020-03-04 14:14:29 +0900

    Add users.views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
+from django.views import View
 from django.shortcuts import render
+from . import forms
+
+
+class LoginView(View):
+    def get(self, request):
+        form = forms.LoginForm()
+        return render(request, "users/login.html", {"form": form})
+
+    def post(self, request):
+        form = forms.LoginForm(request.POST)
+        print(form)
+
 
 # Create your views here.
