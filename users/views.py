ae5c7192c7919c04a481985ecfd27b0d4698ff9d
kih1024 <rladlsgh654@naver.com>
2020-03-23 17:09:15 +0900

    Add valid check function for signup

--- users/views.py
+++ users/views.py
@@ -44,6 +44,27 @@ def log_out(request):
             # print(form.cleaned_data)
         return render(request, "users/login.html", {"form": form}) """
 
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "first_name": "inho",
+        "last_name": "kim",
+        "email": "rladlsgh654@naver.com",
+    }
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+
 # Create your views here.
 # class LoginView(View):
 #     def get(self, request):
