ebaccdbb6862508cc549f0c76475094e99e7eef2
kih1024 <rladlsgh654@naver.com>
2020-04-21 12:11:00 +0900

    Change signup form to modelForm

--- users/forms.py
+++ users/forms.py
@@ -22,6 +22,45 @@ class LoginForm(forms.Form):
             # raise forms.ValidationError("User does not exist")
             self.add_error("email", forms.ValidationError("User does not exist"))
 
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email")
+
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+        if password != password1:
+            raise forms.ValidationError("Password confirmation does not match")
+        else:
+            return password
+
+    def save(self, *args, **kwargs):
+        # object를 생성한것을 데이터 베이스에 안올린다. user는 만들지만 save는 안한다.
+        user = super().save(commit=False)
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        user.username = email
+        user.set_password(password)
+        user.save()
+
+    # ModelForm은 save 메소드를 알아서 제공해준다.
+
+    # def save(self):
+    #     first_name = self.cleaned_data.get("first_name")
+    #     last_name = self.cleaned_data.get("last_name")
+    #     email = self.cleaned_data.get("email")
+    #     password = self.cleaned_data.get("password")
+    #     # 암호화된 비밀번호와 함께 user를 새로 저장한다.
+    #     user = models.User.objects.create_user(email, email, password)
+    #     user.first_name = first_name
+    #     user.last_name = last_name
+    #     user.save()
+
     # 이건 내가 만든 이름이 아니다 . 무엇인가를 이메일이나 비밀번호가 있는 field를 확인 하고 싶으면 method 이름을 clean_ 으로 해야함.
     # def clean_email(self):
     #     # print(self.cleaned_data)
@@ -33,36 +72,36 @@ class LoginForm(forms.Form):
     #         raise forms.ValidationError("User does not exist")
 
 
-class SignUpForm(forms.Form):
-    first_name = forms.CharField(max_length=80)
-    last_name = forms.CharField(max_length=80)
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+# class SignUpForm(forms.Form):
+#     first_name = forms.CharField(max_length=80)
+#     last_name = forms.CharField(max_length=80)
+#     email = forms.EmailField()
+#     password = forms.CharField(widget=forms.PasswordInput)
+#     password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
 
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        try:
-            models.User.objects.get(email=email)
-            raise forms.ValidationError("User already exists with this email")
-        except models.User.DoesNotExist:
-            return email
+#     def clean_email(self):
+#         email = self.cleaned_data.get("email")
+#         try:
+#             models.User.objects.get(email=email)
+#             raise forms.ValidationError("User already exists with this email")
+#         except models.User.DoesNotExist:
+#             return email
 
-    def clean_password1(self):
-        password = self.cleaned_data.get("password")
-        password1 = self.cleaned_data.get("password1")
-        if password != password1:
-            raise forms.ValidationError("Password confirmation does not match")
-        else:
-            return password
+#     def clean_password1(self):
+#         password = self.cleaned_data.get("password")
+#         password1 = self.cleaned_data.get("password1")
+#         if password != password1:
+#             raise forms.ValidationError("Password confirmation does not match")
+#         else:
+#             return password
 
-    def save(self):
-        first_name = self.cleaned_data.get("first_name")
-        last_name = self.cleaned_data.get("last_name")
-        email = self.cleaned_data.get("email")
-        password = self.cleaned_data.get("password")
-        # 암호화된 비밀번호와 함께 user를 새로 저장한다.
-        user = models.User.objects.create_user(email, email, password)
-        user.first_name = first_name
-        user.last_name = last_name
-        user.save()
+#     def save(self):
+#         first_name = self.cleaned_data.get("first_name")
+#         last_name = self.cleaned_data.get("last_name")
+#         email = self.cleaned_data.get("email")
+#         password = self.cleaned_data.get("password")
+#         # 암호화된 비밀번호와 함께 user를 새로 저장한다.
+#         user = models.User.objects.create_user(email, email, password)
+#         user.first_name = first_name
+#         user.last_name = last_name
+#         user.save()
