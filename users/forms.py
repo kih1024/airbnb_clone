ed294af215f946f501b7425321e8b103be03a0b8
kih1024 <rladlsgh654@naver.com>
2020-04-28 13:06:09 +0900

    Finish user Auth

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from . import models
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import password_validation
 
 
 class LoginForm(forms.Form):
 
-    email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
 
     def clean(self):
@@ -28,25 +29,65 @@ class SignUpForm(forms.ModelForm):
         model = models.User
         fields = ("first_name", "last_name", "email")
 
-    password = forms.CharField(widget=forms.PasswordInput)
-    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+    password = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+
+    password1 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        strip=False,
+        help_text="Enter the same password as before, for verification.",
+    )
 
     def clean_password1(self):
         password = self.cleaned_data.get("password")
         password1 = self.cleaned_data.get("password1")
+
         if password != password1:
-            raise forms.ValidationError("Password confirmation does not match")
+            raise forms.ValidationError("Password Error")
         else:
-            return password
+            try:
+                password_validation.validate_password(password1, self.instance)
+                return password
+            except forms.ValidationError as error:
+                self.add_error("password", error)
 
     def save(self, *args, **kwargs):
-        # object를 생성한것을 데이터 베이스에 안올린다. user는 만들지만 save는 안한다.
         user = super().save(commit=False)
-        email = self.cleaned_data.get("email")
+        user.username = self.cleaned_data.get("email")
         password = self.cleaned_data.get("password")
-        user.username = email
         user.set_password(password)
         user.save()
+
+    # username = forms.EmailField(label="Email")
+
+    # class Meta:
+    #     model = models.User
+    #     fields = ("first_name", "last_name", "email")
+
+    # password = forms.CharField(widget=forms.PasswordInput)
+    # password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    # def clean_password1(self):
+    #     password = self.cleaned_data.get("password")
+    #     password1 = self.cleaned_data.get("password1")
+    #     if password != password1:
+    #         raise forms.ValidationError("Password confirmation does not match")
+    #     else:
+    #         return password
+
+    # def save(self, *args, **kwargs):
+    #     # object를 생성한것을 데이터 베이스에 안올린다. user는 만들지만 save는 안한다.
+    #     user = super().save(commit=False)
+    #     email = self.cleaned_data.get("email")
+    #     password = self.cleaned_data.get("password")
+    #     user.username = email
+    #     user.set_password(password)
+    #     user.save()
 
     # ModelForm은 save 메소드를 알아서 제공해준다.
 
