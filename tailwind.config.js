46894795033f67ea6109534b8c664ae27c4cb3b4
kih1024 <rladlsgh654@naver.com>
2020-05-07 17:47:34 +0900

    Tailwind 속성 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      spacing:{
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "75vh": "75vh"
+      },
+      borderRadius: {
+        "xl": "1.5rem"
+      },
+      padding: {
+        "0.8": '0.2rem'
+      }
+    },
   },
   variants: {},
   plugins: [],
