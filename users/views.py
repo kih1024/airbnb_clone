2e014295979417f343eedf91b4ff73b863608f42
kih1024 <rladlsgh654@naver.com>
2020-05-12 16:10:18 +0900

    회원가입 initial 삭제

--- users/views.py
+++ users/views.py
@@ -51,11 +51,6 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
-    initial = {
-        "first_name": "inho",
-        "last_name": "kim",
-        "email": "rladlsgh654@naver.com",
-    }
 
     def form_valid(self, form):
         form.save()
