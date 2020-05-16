d1a558f057a5e4273a2a4b1fa7cecc4967d758e5
kih1024 <rladlsgh654@naver.com>
2020-05-16 13:56:39 +0900

    UserProfileView,UpdateProfileView 추가

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 import os
 import requests
-from django.views import View
-from django.views.generic import FormView, DetailView
+# from django.views import View
+from django.contrib.auth.views import PasswordChangeView
+from django.views.generic import FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
@@ -228,6 +229,26 @@ class UserProfileView(DetailView):
     #     context["hello"] = "Hello!"
     #     return context
 
+
+class UpdateProfileView(UpdateView):
+    model = models.User
+    template_name = "users/update-profile.html"
+    fields = (
+        "first_name",
+        "last_name",
+        "gender",
+        "bio",
+        "birthdate",
+        "language",
+        "currency",
+    )
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+class UpdatePasswordView(PasswordChangeView):
+    
+    template_name = "users/update-password.html"
 
 #                      if user is not None:
 #                         return redirect(reverse("users:login"))
