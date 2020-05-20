9e4f43efeb41cf7ef969b419ed1ec49a60eaf0e5
kih1024 <rladlsgh654@naver.com>
2020-05-20 18:07:20 +0900

    사진 얻는 메소드 추가

--- rooms/models.py
+++ rooms/models.py
@@ -109,6 +109,17 @@ class Room(core_models.TimeStampedModel):
         photo, = self.photos.all()[:1]
         return photo.file.url
 
+    def get_next_four_photos(self):
+        photos = self.photos.all()[1:5]
+        print(photos)
+        return photos
+
+    def get_beds(self):
+        if self.beds == 1:
+            return "1 bed"
+        else:
+            return f"{self.beds} beds"
+
 
 class Photo(core_models.TimeStampedModel):
     """ Photo Model Definition """
