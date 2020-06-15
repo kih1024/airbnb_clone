77462bc18b0d866d79c186973e71f6b123939dbe
kih1024 <rladlsgh654@naver.com>
2020-06-15 14:57:53 +0900

    예약 기능 추가

--- reservations/models.py
+++ reservations/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.utils import timezone
 from core import models as core_models
 
-
 class BookedDay(core_models.TimeStampedModel):
 
     day = models.DateField()
@@ -54,7 +53,10 @@ class Reservation(core_models.TimeStampedModel):
 
     def is_finished(self):
         now = timezone.now().date()
-        return now > self.check_out
+        is_finished = now > self.check_out
+        if is_finished:
+            BookedDay.objects.filter(reservation=self).delete()
+        return is_finished
 
     is_finished.boolean = True
 
