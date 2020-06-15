0781c95ab3c9df04a058b4f78bbc7d1b218bf224
kih1024 <rladlsgh654@naver.com>
2020-06-15 15:00:20 +0900

    core 함수 추가

--- core/models.py
+++ core/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from . import managers
 
 # Create your models here.
 class TimeStampedModel(models.Model):
     """ Time Stamped Model"""
+
     created = models.DateTimeField(auto_now_add=True)
     # 모델을 디비에 적용할때 현재 시간 날짜 저장
     updated = models.DateTimeField(auto_now=True)
-    # 저장 할때마다 updated 모델에 현재 date 그리고 시간 저장 
-
+    # 저장 할때마다 updated 모델에 현재 date 그리고 시간 저장
     # 모델 이지만 데이터베이스에 나타나지 않는 모델
+    
+    objects = managers.CustomModelManager()
+
     class Meta:
         abstract = True
